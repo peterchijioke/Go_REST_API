54622e34ff71da80fd5dd0c756fd981d804bc8ca
peter2c <peter2c@yahoo.com>
2020-08-17 23:13:38 +0100

    get people end point created

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -42,4 +43,30 @@ func personEndPoint(respons http.ResponseWriter, request *http.Request) {
 
 	result, _ := collection.InsertOne(ctx, person)
 	json.NewEncoder(respons).Encode(result)
+}
+
+func getPeopleEndpoint(response http.ResponseWriter, request http.Request) {
+	respons.Header().set("content-type", "application/json")
+	var people []person
+	collection := client.Database("TheNativeDeveloper").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":"` + err.Error() + `s"}`))
+		return
+	}
+	defer cursor.Next(ctx)
+	for cursor.Next(ctx) {
+		var person person
+		cursor.Decode(&person)
+		people = append(people, person)
+	}
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":"` + err.Error() + `s"}`))
+		return
+	}
+	json.NewDecoder(response).Encode(people)
+
 }
