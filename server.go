215d79106a7544400098d2b392aca198ea6c645b
peter2c <peter2c@yahoo.com>
2020-08-17 22:44:46 +0100

    go rest CRUD

--- server.go
+++ server.go
@@ -1,38 +1,45 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// User is a struct that group all field into a single unit
-type User struct {
-	FullName string `json:"fullName"`
-	UserName string `json:"userName"`
-	Email    string `json:"email"`
+type person struct {
+	ID        primitive.ObjectID `json:"_id,omitempy" bson:"_id,omitempty"`
+	firstName string             `json:"firstname,omitempy" bson:"firstname,omitempty"`
+	lastName  string             `json:"lastname,omitempy" bson:"lastname,omitempty"`
 }
+
+var client *mongo.Client
+
+// User is a struct that group all field into a single unit
 
 // post is a struct that group all field into a single unit
-type post struct {
-	Title  string `json:"title"`
-	Body   string `json:"body"`
-	Author User   `json:"author"`
-}
-
-var posts []post = []post{}
 
 func main() {
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ = mongo.Connect(ctx, "mongodb//localhost:2000")
 	r := mux.NewRouter()
-	r.HandleFunc("/add/", addItem).Methods("POST")
+	r.HandleFunc("/person", personEndPoint).Methods("POST")
 	http.ListenAndServe(":5000", r)
 }
 
-func addItem(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var newPost post
-	json.NewDecoder(r.Body).Decode(&newPost)
-	posts = append(posts, newPost)
-	json.NewEncoder(w).Encode(posts)
+func personEndPoint(respons http.ResponseWriter, request *http.Request) {
+	respons.Header().Set("Content-Type", "application/json")
+	var person person
+	json.NewDecoder(request.Body).Decode(&person)
+	collection := client.Database("TheNativeDeveloper").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	result, _ := collection.InsertOne(ctx, person)
+	json.NewEncoder(respons).Encode(result)
 }
